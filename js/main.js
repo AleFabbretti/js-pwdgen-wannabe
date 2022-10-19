7a32ffb7b526e1f7165d536c6a7e1b48c924819b
Alelisk91 <alessandrofabbretti91@gmail.com>
2022-10-19 16:15:38 +0200

    Aggiunti commenti nel file main.js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//chiedere all'utente il suo nome
+
+//chiedere all'utente il suo cognome
+
+//chiedere all'utente il suo colore preferito
+
+//creo una variabile password che contiene la concatenazione delle tre informazioni acquisite precedentemente con l'aggiunta del numero 76
+
+//stampo sulla pagina la password generata
