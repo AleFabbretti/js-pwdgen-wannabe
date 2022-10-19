85dccad25611eefbb25d9a58ab77b932dd44f378
Alelisk91 <alessandrofabbretti91@gmail.com>
2022-10-19 17:14:17 +0200

    Aggiunto variabile al file main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 //chiedere all'utente il suo nome
+const firstName = prompt("inserisci il tuo nome");
 
 //chiedere all'utente il suo cognome
+const lastName = prompt("inserisci il tuo cognome");
 
 //chiedere all'utente il suo colore preferito
+const favouriteColor = prompt("inserisci il tuo colore preferito");
 
 //creo una variabile password che contiene la concatenazione delle tre informazioni acquisite precedentemente con l'aggiunta del numero 76
+const password = `${firstName}${lastName}${favouriteColor}76`;
 
-//stampo sulla pagina la password generata
+console.log(password);
+
